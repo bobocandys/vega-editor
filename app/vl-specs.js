2b186c3e6b2266d10152e106ef104d5afaeb85e1
Youying Lin <bobocandys@gmail.com>
2016-06-08 00:20:59 -0700

    Add more examples

--- app/vl-specs.js
+++ app/vl-specs.js
@@ -1,1 +1,3 @@
-var VL_SPECS = { "Basic": [ { "name": "airportSized", "title": "example1", "galleryParameters": { "backgroundPosition": "25% 50%" } }] }
+var VL_SPECS = { "Basic": [ { "name": "airportSized", "title": "airports with features of counties", "galleryParameters": { "backgroundPosition": "25% 50%" }} ,
+{ "name": "geoTransform", "title": "geo transform", "galleryParameters": { "backgroundPosition": "25% 50%" }} ,
+{ "name": "geopathTransform", "title": "geopath transform", "galleryParameters": { "backgroundPosition": "25% 50%" } }] }
