797d93b6e904e632b0a7d19502f801a9c51adf1a
Youying Lin <bobocandys@gmail.com>
2016-06-08 00:25:07 -0700

    Switch features for geopath

--- app/vl-specs.js
+++ app/vl-specs.js
@@ -1,3 +1,3 @@
-var VL_SPECS = { "Basic": [ { "name": "airportsSized", "title": "airports with features of counties", "galleryParameters": { "backgroundPosition": "25% 50%" }} ,
+var VL_SPECS = { "Basic": [ { "name": "airportsSized", "title": "airports with features of states", "galleryParameters": { "backgroundPosition": "25% 50%" }} ,
 { "name": "geoTransform", "title": "geo transform", "galleryParameters": { "backgroundPosition": "25% 50%" }} ,
 { "name": "geopathTransform", "title": "geopath transform", "galleryParameters": { "backgroundPosition": "25% 50%" } }] }
